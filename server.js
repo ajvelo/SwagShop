a3c12fe5d33850feab36d86727b148eb0c2b5316
Andreas Velounias <andreas.velounias@rokigroup.co.uk>
2019-01-03 11:10:36 +0000

    Add nodemon

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(3000, function() {
+    console.log('Running on port 3000');
+});
