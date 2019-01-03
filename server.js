9fdff79e766675d40c4ff680630d0ef8d04b8aa1
Andreas Velounias <andreas.velounias@rokigroup.co.uk>
2019-01-03 11:32:09 +0000

    Add product model

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/swap-shop');
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(3000, function() {
-    console.log('Running on port 3000');
+    console.log('Running on port 3000..');
 });
