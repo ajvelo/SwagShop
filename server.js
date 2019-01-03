fdeb0e19030062f2650f96997c727ff8ffcaa471
Andreas Velounias <andreas.velounias@rokigroup.co.uk>
2019-01-03 11:40:39 +0000

    Import modules

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/swap-shop');
 
+var product = require('./model/product');
+var wishlist = require('./model/wishlist');
+
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
